9738f9961ce91cac1f52db85fe38073546713a6b
hatuank97lhp <69759128+hatuank97lhp@users.noreply.github.com>
2023-02-07 08:35:13 +0700

    update main.py

--- docker-hadoop-spark/src/main.py
+++ docker-hadoop-spark/src/main.py
@@ -9,8 +9,12 @@ from operator import add
 import sys,os
 from pyspark.sql.types import *
 
-import config
+import config, io_cluster
 
+# schema = StructType([
+#       StructField("user_id",StringType(),True),
+#       StructField("username",StringType(),True)
+#   ])
 
 if __name__ == "__main__":
     
@@ -29,61 +33,46 @@ if __name__ == "__main__":
     spark = app_config.initialize_spark_session(APP_NAME)
     print("read file csv")
     print("------------------------------------------------------------------------------------------------------")
-    # sc = spark.sparkContext
-    # sc.addPyFile(os.path.dirname(__file__)+"/patterns.py")
-    df_user = spark.read.csv("hdfs://namenode:9000/data/users/user.csv")
-    print("read user done!")
-    print("------------------------------------------------------------------------------------------------------")
-    df_user_0 = spark.read.csv("hdfs://namenode:9000/data/users/0.csv")
-    
-    def count_episode_watched():
-        sql = spark.sql("Select u.username, count(u_0.watched_episodes) as total_episode_watched\
-                         From user u, 0 u_0\
-                         Where u.user_id = 0")
-        
-        sql.show()
-    
-    count_episode_watched()
-    # raw_recruit_df = spark.read.schema(schema).option("multiline","true").json("hdfs://namenode:9000/data/rawdata/*.json")
-    # # raw_recruit_df.show(5)
-    # extracted_recruit_df=raw_recruit_df.select(raw_recruit_df["name"].alias("CompanyName"),
-    #       udfs.extract_framework_plattform("Mô tả công việc","Yêu cầu ứng viên").alias("FrameworkPlattforms"),
-    #       udfs.extract_language("Mô tả công việc","Yêu cầu ứng viên").alias("Languages"),
-    #       udfs.extract_design_pattern("Mô tả công việc","Yêu cầu ứng viên").alias("DesignPatterns"),
-    #       udfs.extract_knowledge("Mô tả công việc","Yêu cầu ứng viên").alias("Knowledges"),
-    #       udfs.normalize_salary("Quyền lợi").alias("Salaries")
-    #       )
-    # extracted_recruit_df.cache()
-    # # extracted_recruit_df.show(5)
 
-    # ##========save extracted_recruit_df to hdfs========================
-    # df_to_hdfs=(extracted_recruit_df,)
-    # df_hdfs_name = ("extracted_recruit.json",)
-    # io_cluster.save_dataframes_to_hdfs("data/extracteddata", app_config, df_to_hdfs, df_hdfs_name)
+    df_anime_list = spark.read.csv("hdfs://namenode:9000/data/animelist.csv")
+    df_anime_list.createOrReplaceTempView("animelist")
 
-    # ##========make some query==========================================
-    # knowledge_df = queries.get_counted_knowledge(extracted_recruit_df)
-    # knowledge_df.cache()
-    # # knowledge_df.show(5)
+    df_anime = spark.read.csv("hdfs://namenode:9000/data/anime.csv")
+    df_anime.createOrReplaceTempView("anime")
 
-    # udfs.broadcast_labeled_knowledges(sc,patterns.labeled_knowledges)
-    # grouped_knowledge_df = queries.get_grouped_knowledge(knowledge_df)
-    # grouped_knowledge_df.cache()
-    # # grouped_knowledge_df.show()
+    df_rating = spark.read.csv("hdfs://namenode:9000/data/rating_complete.csv")
+    df_rating.createOrReplaceTempView("rating")
 
-    # extracted_recruit_df = extracted_recruit_df.drop("Knowledges")
-    # extracted_recruit_df.cache()
+    df_genres = spark.read.csv("hdfs://namenode:9000/data/genres.csv")
+    df_genres.createOrReplaceTempView("genres")
 
-    # ##========save some df to elasticsearch========================
-    # df_to_elasticsearch=(
-    #                      extracted_recruit_df,
-    #                     #  knowledge_df,
-    #                      grouped_knowledge_df
-    #                      )
-    
-    # df_es_indices = (
-    #                  "recruit",
-    #               #    "knowledges",
-    #                  "grouped_knowledges"
-    #                  )
-    # io_cluster.save_dataframes_to_elasticsearch(df_to_elasticsearch,df_es_indices,app_config.get_elasticsearch_conf())
+    #1 add animelist
+    anime_list = spark.sql("Select a._c0 as userId, a._c1 as animeId, a._c2 as rating, a._c3 as watchingStatus, a._c4 as watchedEpisodes from animelist a ")
+
+    #2 add anime
+    anime = spark.sql("Select a._c0 as animeId, a._c1 as name, a._c2 as scores, a._c3 as genres, a._c6 as type, a._c7 as episodes, a._c12 as studio, a._c13 as source, a._c15 as rating, a._c16 as ranking from anime a")
+
+    #3 add rating
+    rating = spark.sql("Select r._c0 as userId, r._c1 as animeId, r._c2 as rating from rating r")
+
+    #4 add animelist join anime
+    animeTotalWatches =  spark.sql("Select alist._c1 as animeId, sum(alist._c4) as totalWatchedEpisodes, a._c1 as name, a._c2 as scores, a._c3 as genres, a._c6 as type, a._c7 as episodes, a._c12 as studio, a._c13 as source, a._c15 as rating, a._c16 as ranking from anime a, animelist alist where a._c0 = alist._c1 group by alist._c1, a._c1, a._c2, a._c3, a._c6, a._c7, a._c12, a._c13, a._c15, a._c16")
+    animeTotalWatches.createOrReplaceTempView("total_views_anime")
+
+    #5 add userTotalWatched
+    userTotalWatched = spark.sql("Select a._c0 as userId, sum(a._c4) as userWatchedEpisodes from animelist a group by a._c0")
+
+    #6 animelist 100 users
+    top_100_users = spark.sql("Select al._c0 as userId, al._c1 as animeId, al._c2 as rating, al._c3 as watchingStatus, al._c4 as watchedEpisodes, a._c0 as animeId, a._c1 as name, a._c2 as scores, a._c3 as genres, a._c6 as type, a._c7 as episodes, a._c12 as studio, a._c13 as source, a._c15 as rating, a._c16 as ranking from animelist al, anime a where al._c0 < 101 and al._c1 = a._c0")
+
+
+    # genres_header.append("TotalEps")
+    # print("genres header: ", genres_header)
+
+    io_cluster.save_dataframes_to_elasticsearch(
+        (anime, rating, animeTotalWatches, userTotalWatched),
+        ("anime", "rating", "anime_total_watches", "user_total_watched"),
+        app_config.get_elasticsearch_conf()
+    )
+
+    # io_cluster.save_dataframes_to_elasticsearch2(top_100_users, "top_100_users", app_config.get_elasticsearch_conf())
